b5d875b516d952e01b9456d15de684909246b116
deanbunn <deanbunn@gmail.com>
2022-02-24 08:22:18 -0800

    20220224_0821hrs Testing add permissions

--- GPOs_Add_Permissions.ps1
+++ GPOs_Add_Permissions.ps1
@@ -1,11 +1,17 @@
 <#
 	Script: GPOs_Add_Permissions.ps1
 	Author: Taylor McDougall and Dean Bunn
-	Last Edited: 2022-02-23
+	Last Edited: 2022-02-24
 #>
 
 #Import Group Policy Module 
-#Import-Module GroupPolicy;
+Import-Module GroupPolicy;
+
+#Var for Domain and Name of Admin Group to Grant Permissions
+[string]$grantedAdminGroup = "AD3\COE-Admins";
+
+#Var for Domain Server
+[string]$dmnServer = "ou.ad3.ucdavis.edu";
 
 #Check for GPO Report File
 $csvGPOs = Import-Csv -Path .\Report-GPOs.csv;
@@ -14,14 +20,20 @@ foreach($csvGPO in $csvGPOs)
 {
 
     #Only Grant Rights to GPOs the Old Group had Full Rights On
-    if([string]::IsNullOrEmpty($csvGPO.PermissionLevel) -eq $false -and $csvGPO.PermissionLevel -eq "GpoEditDeleteModifySecurity")
+    if([string]::IsNullOrEmpty($csvGPO.Id) -eq $false -and [string]::IsNullOrEmpty($csvGPO.PermissionLevel) -eq $false -and $csvGPO.PermissionLevel -eq "GpoEditDeleteModifySecurity")
     {
         #Setup for Testing
         if($csvGPO.Id -eq "5885a54e-26f5-424a-9130-ce4c9140ce65")
         {
-            $csvGPO.DisplayName;
-        }
+            #Convert String to Guid
+            $guidGPOID = [Guid]$csvGPO.Id;
+
+            #Add Full Permissions to GPO for Admin Group
+            Set-GPPermission -Guid $guidGPOID -TargetName $grantedAdminGroup -TargetType Group -Server $dmnServer -PermissionLevel GpoEditDeleteModifySecurity;
+
+        }#End of Setup for Testing
           
     }#End of Permission Level Check for Old Group
     
 }#End of $csvGPOs Foreach
+
--- README.md
+++ README.md
@@ -4,15 +4,11 @@ This project solution is based upon two scripts.
 
 One to report the current permission level granted to a specific AD group on each GPO assigned to OUs in a department OU.
 
-The second script will take the GPO Ids from the report of the first script and assign edit and modify security rights to another AD group
+The second script will add permissions for the new admin group by taking the GPO Ids from the report of the first script and configuring the required settings.
 
 
-### Required Setup
+### Requirements
 
-The PowerShell Active Directory Module must be installed on the system.
+The PowerShell Active Directory and Group Policy Modules must be installed on the system.
 
-```powershell
-# On Windows 10 systems
-Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
-```
 
